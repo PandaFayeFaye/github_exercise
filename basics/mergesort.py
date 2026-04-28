f10fb0c9b6f1c0a7e957f54d2ede051552a9af6a
PandaFayeFaye <fayexie425@gmail.com>
2026-04-28 21:05:33 +0300

    use original course mergesort.py

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,42 +1,39 @@
-def merge(left, right):
+def merge(a, b):
     print("Merging...")
-    print(f"left: {left}")
-    print(f"right: {right}")
-    merged = []
-    i = j = 0
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            merged.append(left[i])
-            i += 1
+    print("left:", a)
+    print("right:", b)
+    c = []
+    while len(a) > 0 and len(b) > 0:
+        if a[0] < b[0]:
+            c.append(a.pop(0))
         else:
-            merged.append(right[j])
-            j += 1
-    merged.extend(left[i:])
-    merged.extend(right[j:])
-    print(f"merged: {merged}")
-    return merged
+            c.append(b.pop(0))
+    c += a
+    c += b
+    print("merged:", c)
+    return c
 
 
-def mergesort(arr):
+def msort(arr):
     if len(arr) <= 1:
         return arr
-    print(f"array: {arr}")
+    print("array:", arr)
     m = len(arr) // 2
-    print(f"m: {m}")
-    left = mergesort(arr[:m])
-    right = mergesort(arr[m:])
+    print("m:", m)
+    left = msort(arr[:m])
+    right = msort(arr[m:])
     return merge(left, right)
 
 
 if __name__ == "__main__":
     print("Enter numbers, separated by ',':", end=" ")
-    s = input().strip()
+    s = input()
     input_list = s.split(",")
     input_list = [x.strip() for x in input_list]
-    print(f"input_list: {input_list}")
+    print("input_list:", input_list)
 
     value_list = [int(x) for x in input_list]
-    print(f"value_list: {value_list}")
+    print("value_list:", value_list)
 
-    result = mergesort(value_list)
+    result = msort(value_list)
     print(result)
